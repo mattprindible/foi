88f3534c837448a696c1a6191ea679bffea40dc7
Matt Prindible <service+github@mattprindible.com>
2021-04-14 23:53:54 -0400

    airtable

--- _data/all.js
+++ _data/all.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 const Airtable = require('airtable');
 
-//let base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appi9lQAVFMGTbT8J');
+let base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appi9lQAVFMGTbT8J');
