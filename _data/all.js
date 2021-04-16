31652acbd6c8cc37ad77623c36f70f66226e9a06
Matt Prindible <service+github@mattprindible.com>
2021-04-15 23:39:22 -0400

    get from airtable

--- _data/all.js
+++ _data/all.js
@@ -2,3 +2,28 @@ require('dotenv').config();
 const Airtable = require('airtable');
 
 let base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appi9lQAVFMGTbT8J');
+
+module.exports = () => {
+    return new Promise((resolve, reject) => {
+        const allData = [];
+        base('Dump')
+            .select({ view: 'All' })
+            .eachPage(
+                function page(records, fetchNextPage) {
+                    records.forEach((record) => {
+                        allData.push({
+                            "id" : record._rawJson.id, ...record._rawJson.fields
+                        });
+                    });
+                    fetchNextPage();
+                },
+                function done(err) {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(allData);
+                    }
+                }
+            );
+    });
+};
